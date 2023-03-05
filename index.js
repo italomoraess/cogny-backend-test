55347c69793bc10e838336964e60bfe442087e06
italomoraess <italomoraes.sth@gmail.com>
2023-03-04 23:03:55 -0300

    add function sumPopulation and sumPopulationSql

--- index.js
+++ index.js
@@ -67,16 +67,24 @@ const urlApi = 'https://datausa.io/api/data?drilldowns=Nation&measures=Populatio
     try {
         await migrationUp();
 
-        const requestData = async () => {
-            const { data: { data } } = await axios.get(urlApi);
-            return data;
-        };
+        // const requestData = async () => {
+        //     const { data: { data } } = await axios.get(urlApi);
+        //     console.log(data);
+        //     return data;
+        // };
 
-        console.log('Data => ', data);
+        // const data = await requestData();
+
+        const data = [{"ID Nation":"01000US","Nation":"United States","ID Year":2020,"Year":"2020","Population":326569308,"Slug Nation":"united-states"},
+        {"ID Nation":"01000US","Nation":"United States","ID Year":2019,"Year":"2019","Population":324697795,"Slug Nation":"united-states"},
+        {"ID Nation":"01000US","Nation":"United States","ID Year":2018,"Year":"2018","Population":322903030,"Slug Nation":"united-states"},
+        {"ID Nation":"01000US","Nation":"United States","ID Year":2017,"Year":"2017","Population":321004407,"Slug Nation":"united-states"},
+        {"ID Nation":"01000US","Nation":"United States","ID Year":2016,"Year":"2016","Population":318558162,"Slug Nation":"united-states"},
+        {"ID Nation":"01000US","Nation":"United States","ID Year":2015,"Year":"2015","Population":316515021,"Slug Nation":"united-states"},
+        {"ID Nation":"01000US","Nation":"United States","ID Year":2014,"Year":"2014","Population":314107084,"Slug Nation":"united-states"},
+        {"ID Nation":"01000US","Nation":"United States","ID Year":2013,"Year":"2013","Population":311536594,"Slug Nation":"united-states"}];
 
         //exemplo de insert
-        const data = await requestData();
-
         data.map(async (d) => {
             await db[DATABASE_SCHEMA].api_data.insert({
                 doc_record: d,
@@ -84,10 +92,31 @@ const urlApi = 'https://datausa.io/api/data?drilldowns=Nation&measures=Populatio
         });
 
         //exemplo select
-        // const result2 = await db[DATABASE_SCHEMA].api_data.find({
-        //     is_active: true
-        // });
-        // console.log('result2 >>>', result2);
+        const sumPopulation = () => {
+            let totalPopulation = 0;
+            data.map((d) => {
+                if (d.Year >= 2018 && d.Year <= 2020) {
+                    totalPopulation += d.Population;
+                };
+            });
+
+            return totalPopulation.toLocaleString('pt-BR');
+        }
+
+        const sumPopulationSql = async () => {
+            const [{ sum_population }] = await db.query(`
+            SELECT SUM((doc_record->> 'Population')::int) as sum_population
+            FROM italomoraess.api_data
+            WHERE doc_record->>'Year' IN ('2018', '2019', '2020')
+            `);
+
+            return Number(sum_population).toLocaleString('pt-BR');
+        }
+        
+        console.log("Total Population JS =>", await sumPopulation())
+        console.log("Total Population SQL =>", await sumPopulationSql())
+
+        //await db[DATABASE_SCHEMA].api_data.destroy({});
 
     } catch (e) {
         console.log(e.message)
